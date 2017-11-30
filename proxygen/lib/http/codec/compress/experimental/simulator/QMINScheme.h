4500c033c045f96c5aa07013838975d41e63ebff
Dmitri Tikhonov <dtikhonov@live.com>
2017-11-30 10:54:10 -0500

    Add enc/dec and dec/enc control channels
    Throw away messages for now

--- proxygen/lib/http/codec/compress/experimental/simulator/QMINScheme.h
+++ proxygen/lib/http/codec/compress/experimental/simulator/QMINScheme.h
@@ -15,13 +15,21 @@ class QMINScheme : public CompressionScheme {
   explicit QMINScheme(CompressionSimulator* sim)
       : CompressionScheme(sim)
   {
-    qms_enc = qmin_enc_new(QSIDE_CLIENT, 64 * 1024, NULL);
+    qms_ctl[0].qco_write = write_enc2dec;
+    qms_ctl[0].qco_ctx = this;
+    qms_ctl[1].qco_write = write_dec2enc;
+    qms_ctl[0].qco_ctx = this;
+
+    qms_enc = qmin_enc_new(QSIDE_CLIENT, 64 * 1024, &qms_ctl[0]);
+    qms_dec = qmin_dec_new(QSIDE_SERVER, 64 * 1024, &qms_ctl[1]);
+
     qms_next_stream_id_to_encode = 1;
   }
 
   ~QMINScheme()
   {
     qmin_enc_destroy(qms_enc);
+    qmin_dec_destroy(qms_dec);
   }
 
   std::unique_ptr<CompressionScheme::Ack> getAck(uint16_t seqn) override
@@ -83,11 +91,26 @@ class QMINScheme : public CompressionScheme {
     return;
   }
 
-  struct qmin_enc *qms_enc;
+  static void write_enc2dec (void *qco_ctx, const void *buf, size_t sz)
+  {
+    VLOG(1) << "Throw away encoder-to-decoder control message";
+  }
+
+  static void write_dec2enc (void *qco_ctx, const void *buf, size_t sz)
+  {
+    VLOG(1) << "Throw away decoder-to-encoder control message";
+  }
+
+  struct qmin_ctl_out   qms_ctl[2]; /* 0: encoder-to-decoder;
+                                     * 1: decoder-to-encoder.
+                                     */
+
+  struct qmin_enc      *qms_enc;
+  struct qmin_dec      *qms_dec;
 
   /* Each call to `encode' is interpreted as a header block for a new
    * stream.
    */
-  unsigned         qms_next_stream_id_to_encode;
+  unsigned              qms_next_stream_id_to_encode;
 };
 }}
