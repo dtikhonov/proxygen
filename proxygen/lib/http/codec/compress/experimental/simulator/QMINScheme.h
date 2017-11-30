ac32a27f35aca3db91ed2711093f333d03a9c198
Dmitri Tikhonov <dtikhonov@live.com>
2017-11-30 15:13:56 -0500

    Encode stream ID and control messages into the header block
    Encoding the control messages into the header block emulates a
    separate control channel.

--- proxygen/lib/http/codec/compress/experimental/simulator/QMINScheme.h
+++ proxygen/lib/http/codec/compress/experimental/simulator/QMINScheme.h
@@ -46,24 +46,28 @@ class QMINScheme : public CompressionScheme {
   std::pair<bool, std::unique_ptr<folly::IOBuf>> encode(
     std::vector<compress::Header> allHeaders, SimStats& stats) override
   {
-    unsigned char outbuf[0x1000];
-    size_t nw, off;
+    const size_t max_ctl = 0x1000;
+    const size_t max_comp = 0x1000;
+    unsigned char outbuf[max_ctl + max_comp];
+    unsigned char *const comp = outbuf + max_ctl;
+    size_t nw, comp_sz;
     enum qmin_encode_status qes;
 
-    off = 0;
+    qms_ctl_off = 0;
+    comp_sz = 0;
 
     for (const auto header : allHeaders) {
       std::string name{header.name->c_str()};
       std::transform(name.begin(), name.end(), name.begin(), ::tolower);
       qes = qmin_enc_encode(qms_enc, qms_next_stream_id_to_encode, name.c_str(),
         name.length(), header.value->c_str(), header.value->length(), QIT_YES,
-        outbuf + off, sizeof(outbuf) - off, &nw);
+        comp + comp_sz, max_comp - comp_sz, &nw);
       switch (qes)
       {
       case QES_OK:
         stats.uncompressed += name.length() + header.value->length();
         stats.compressed += nw;
-        off += nw;
+        comp_sz += nw;
         break;
       case QES_NOBUFS:
         VLOG(1) << "compressed header does not fit into temporary output buffer";
@@ -74,20 +78,61 @@ class QMINScheme : public CompressionScheme {
       }
     }
 
+    /* Prepend control message and its size: */
+    if (qms_ctl_off)
+      memcpy(outbuf + max_ctl - qms_ctl_off, qms_ctl_buf, qms_ctl_off);
+    memcpy(outbuf + max_ctl - qms_ctl_off - sizeof(qms_ctl_off), &qms_ctl_off,
+           sizeof(qms_ctl_off));
+
+    /* Prepend Stream ID: */
+    memcpy(outbuf + max_ctl - qms_ctl_off - sizeof(qms_ctl_off) - sizeof(uint32_t),
+           &qms_next_stream_id_to_encode, sizeof(qms_next_stream_id_to_encode));
+
     qms_next_stream_id_to_encode += 2;
-    return {true, folly::IOBuf::copyBuffer(outbuf, off)};
+    return {true, folly::IOBuf::copyBuffer(outbuf + max_ctl - qms_ctl_off -
+      sizeof(qms_ctl_off) - sizeof(uint32_t),
+      comp_sz + qms_ctl_off + sizeof(qms_ctl_off) + sizeof(uint32_t))};
   }
 
   void decode(bool, std::unique_ptr<folly::IOBuf> encodedReq,
               SimStats&, SimStreamingCallback& callback) override
   {
     folly::io::Cursor cursor(encodedReq.get());
-    const unsigned char *buf = cursor.data();
-    const unsigned char *const end = buf + cursor.length();
+    const unsigned char *buf;
     ssize_t nread;
+    size_t ctl_sz;
     char outbuf[0x1000];
     unsigned name_len, val_len;
     unsigned decoded_size = 0;
+    uint32_t stream_id;
+
+    qms_ctl_off = 0;
+
+    /* Read Stream ID: */
+    buf = cursor.data();
+    memcpy(&stream_id, buf, sizeof(uint32_t));
+    encodedReq->trimStart(sizeof(uint32_t));
+
+    /* Read size of control messages */
+    buf = cursor.data();
+    memcpy(&ctl_sz, buf, sizeof(ctl_sz));
+    encodedReq->trimStart(sizeof(ctl_sz));
+
+    /* Feed control messages to the decoder: */
+    if (ctl_sz)
+    {
+      buf = cursor.data();
+      nread = qmin_dec_cmds_in(qms_dec, buf, ctl_sz);
+      if (nread < 0 || (size_t) nread != ctl_sz)
+      {
+        VLOG(1) << "oops: could not get all commands in";
+        return;
+      }
+      encodedReq->trimStart(ctl_sz);
+    }
+
+    buf = cursor.data();
+    const unsigned char *const end = buf + cursor.length();
 
     while (buf < end)
     {
@@ -119,14 +164,30 @@ class QMINScheme : public CompressionScheme {
     CompressionScheme::runLoopCallback();
   }
 
-  static void write_enc2dec (void *qco_ctx, const void *buf, size_t sz)
+  void write_ctl_msg (const void *buf, size_t sz)
   {
-    VLOG(1) << "Throw away encoder-to-decoder control message";
+    size_t avail = sizeof(qms_ctl_buf) - qms_ctl_off;
+    if (avail < sz)
+    {
+      VLOG(1) << "Truncating control message from " << sz << " to "
+              << avail << "bytes";
+      sz = avail;
+    }
+    memcpy(qms_ctl_buf + qms_ctl_off, buf, sz);
+    qms_ctl_off += sz;
+    VLOG(4) << "Wrote " << sz << " bytes to control channel";
   }
 
-  static void write_dec2enc (void *qco_ctx, const void *buf, size_t sz)
+  static void write_enc2dec (void *ctx, const void *buf, size_t sz)
   {
-    VLOG(1) << "Throw away decoder-to-encoder control message";
+    QMINScheme *const qms = (QMINScheme *) ctx;
+    qms->write_ctl_msg(buf, sz);
+  }
+
+  static void write_dec2enc (void *ctx, const void *buf, size_t sz)
+  {
+    QMINScheme *const qms = (QMINScheme *) ctx;
+    qms->write_ctl_msg(buf, sz);
   }
 
   struct qmin_ctl_out   qms_ctl[2]; /* 0: encoder-to-decoder;
@@ -140,5 +201,8 @@ class QMINScheme : public CompressionScheme {
    * stream.
    */
   unsigned              qms_next_stream_id_to_encode;
+
+  unsigned char         qms_ctl_buf[0x1000];
+  size_t                qms_ctl_off;
 };
 }}
